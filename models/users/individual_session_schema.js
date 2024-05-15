3d2e579bd9b96bf5a7e153c651a05763d6f3d14e
Leo0901 <leozheng.inbox@gmail.com>
2024-05-14 18:54:16 -0700

    Renamed homepage, loginerror, and signup to fit the naming convention. Created database schema modules for achievements, costumes, group study sessions, individual study sessions, groups, users, and pets

---
+++ models/users/individual_session_schema.js
@@ -0,0 +1,30 @@
+const individual_session_schema = {
+  $jsonSchema: {
+    bsonType: "object",
+    required: ["user_id", "hours_spent", "date", "points_earned"],
+    properties: {
+      _id: {
+        bsonType: "objectId",
+        description: "must be an ObjectId and is required",
+      },
+      user_id: {
+        bsonType: "objectId",
+        description: "must be an ObjectId and is required",
+      },
+      hours_spent: {
+        bsonType: "int",
+        description: "must be a number and is required",
+      },
+      date: {
+        bsonType: "date",
+        description: "must be a date and is required",
+      },
+      points_earned: {
+        bsonType: "int",
+        description: "must be a number and is required",
+      },
+    },
+  },
+};
+
+module.exports = individual_session_schema;
---
+++ models/users/user_schema.js
@@ -0,0 +1,91 @@
+const user_schema = {
+  $jsonSchema: {
+    bsonType: "object",
+    required: [
+      "username",
+      "email",
+      "password",
+      "total_study_hours",
+      "points",
+      "pets_owned",
+      "current_pet",
+    ],
+    properties: {
+      _id: {
+        bsonType: "objectId",
+        description: "must be an ObjectId and is required",
+      },
+      username: {
+        bsonType: "string",
+        description: "must be a string and is required",
+      },
+      email: {
+        bsonType: "string",
+        pattern: "^.+@.+..+$",
+        description: "must be a string and match the email pattern",
+      },
+      password: {
+        bsonType: "string",
+        description: "must be a string and is required",
+      },
+      total_study_hours: {
+        bsonType: "int",
+        description: "must be a number and is required",
+      },
+      points: {
+        bsonType: "int",
+        description: "must be a number and is required",
+      },
+      pets_owned: {
+        bsonType: "array",
+        items: {
+          bsonType: "objectId",
+        },
+        description: "must be an array of pet IDs and is required",
+      },
+      friends: {
+        bsonType: "array",
+        items: {
+          bsonType: "objectId",
+        },
+        description: "must be an array of friends' IDs",
+      },
+      individual_sessions: {
+        bsonType: "array",
+        items: {
+          bsonType: "objectId",
+        },
+        description:
+          "must be an array of session IDs for individual study sessions",
+      },
+      group_sessions: {
+        bsonType: "array",
+        items: {
+          bsonType: "objectId",
+        },
+        description: "must be an array of session IDs for group study sessions",
+      },
+      current_pet: {
+        bsonType: "objectId",
+        description: "must be an ObjectId",
+      },
+      study_streak: {
+        bsonType: "int",
+        description: "must be a number",
+      },
+      equip_profile_image: {
+        bsonType: "string",
+        description: "must be a string",
+      },
+      achievements: {
+        bsonType: "array",
+        items: {
+          bsonType: "objectId",
+        },
+        description: "must be an array of achievement IDs",
+      },
+    },
+  },
+};
+
+module.exports = user_schema;
