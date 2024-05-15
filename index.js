f6902adfc78e7180bc47ca1d41dbb8668b74f308
Alvin Hu <alvehnhu@gmail.com>
2024-05-14 18:26:24 -0700

    added headers and footers to all ejs pages and added friends and group page

--- index.js
+++ index.js
@@ -135,6 +135,10 @@ app.get('/homepage', (req, res) => {
     res.render('homepage');
 })
 
+app.get('/friends', (req, res) => {
+    res.render('friends');
+})
+
 app.get('/profile', (req, res) => {
     res.render('profile');
 })
